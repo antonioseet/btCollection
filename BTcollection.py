3f37af99456a04287bf812b0c5ae9d44848c90d5
Tony Raspberry Pi <antonioseet1@gmail.com>
2019-04-28 05:55:58 +0000

    Fixed text splitting issue. Optimized and re-organized code. Removed all unnecessary print statements.

--- BTcollection.py
+++ BTcollection.py
@@ -1,77 +1,48 @@
 import bluetooth
 import time
-
-
         
 def main():
     # Read source files and convert into lists
-    with open("BTaddresses.txt", "r") as addresses:
-        with open("BTnames.txt", "r") as names:
-            # List part
-            addressFile = addresses.read()
-            addressesArray = addressFile.split()
-
-            namesFile = names.read()
-            namesArray = namesFile.split()
-            print("Names in File = "+str(namesArray))
-            
-            #Infinite loop that looks for new devices until program is stopped.            timesProgramRan = 1
-            while True:
-
-                nearby_devices = bluetooth.discover_devices(duration=2, lookup_names=True)
-                print(str(nearby_devices)+" Devices Found during BT SEARCH")
-                for addr, name in nearby_devices:
-                    if(addr not in addressesArray):
-                       addressesArray.append(addr)
-                    if(name not in namesArray):
-                        namesArray.append(name)
-
-                print("Processing... " + str(addressesArray))
-                print("Processing... " + str(namesArray))
-                writeFiles(namesArray, addressesArray)
-
-                
-                
-                print("Times program ran = " + str(timesProgramRan))
-                timesProgramRan = timesProgramRan + 1
-                time.sleep(2)
+    addressesArray = [line.rstrip('\n') for line in open("BTaddresses.txt", "r")]
+    namesArray = [line.rstrip('\n') for line in open("BTnames.txt", "r")]
+    timesProgramRan = 1
     
+    #Infinite loop that looks for new devices until program is stopped.            timesProgramRan = 1
+    while True:
+        nearby_devices = bluetooth.discover_devices(duration=2, lookup_names=True)
+        print "Discoverable devices in range" + str(nearby_devices)
+        for addr, name in nearby_devices:
+            if(addr not in addressesArray):
+                addressesArray.append(addr)
+            if(name not in namesArray):
+                print "NEW DEVICE: " + str(name)
+                namesArray.append(name)
+                
+        writeFiles(namesArray, addressesArray)
+                
+        print("Times program ran = " + str(timesProgramRan))
+        timesProgramRan = timesProgramRan + 1
+        time.sleep(2)
 
 
 # update names and addresses        
 def writeFiles(namesList, addressList):
-    print("Saving...")
+    print "Saving..."
     with open("BTaddresses.txt", "r+") as addressFile:
-        with open("BTnames2.txt", "r+") as namesFile:
+        with open("BTnames.txt", "r+") as namesFile:
                 addressFile.truncate()
                 namesFile.truncate()
                 nameEntries = len(namesList)
                 addressEntries = len(addressList)
-                print(str(nameEntries) + " names to record. And " + str(addressEntries) + " addresses")
-
-                #if(numEntries > 0):
-                print(str(namesList) + "|" + str(addressList))
 
                     #Rewrite the BTaddress.txt & the BTnames.txt
                 for i in range(nameEntries):
-                    print ("i = " +str(i))
-                    if i == nameEntries-1: #(is this the last item we are recording?)
+                    if i == nameEntries-1: # Add new line if more items pending
                         namesFile.write(namesList[i])
-                    else:
-                        namesFile.write(namesList[i] + "\n")
-
-                for i in range(addressEntries):
-                    print(i)
-                    if i == addressEntries - 1:
                         addressFile.write(addressList[i])
                     else:
+                        namesFile.write(namesList[i] + "\n")
                         addressFile.write(addressList[i] + "\n")
-                print("SAVED!\n")
-
-
-
-
-                    
-
+                print "SAVED!\n"
 
 main()
