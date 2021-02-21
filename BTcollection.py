3c9c60bb7076623fb4fff551b57cb63f31caa3e9
Tony Barrera <antonioseet1@gmail.com>
2021-02-20 20:46:45 -0800

    Mac Commit - comment out BT functionality.

--- BTcollection.py
+++ BTcollection.py
@@ -1,22 +1,30 @@
-import bluetooth
+#import bluetooth
 import time
         
 def main():
-    # Read source files and convert into lists
+    # Read source files and convert into two lists, one for Bluetooth adresses and one for names. 
+    ## WILL REPLACE WITH TUPLES or USER class list.
+    ### This should also take into account the points lists.
     addressesArray = [line.rstrip('\n') for line in open("BTaddresses.txt", "r")]
     namesArray = [line.rstrip('\n') for line in open("BTnames.txt", "r")]
     timesProgramRan = 1
     
+    """
     #Infinite loop that looks for new devices until program is stopped.            timesProgramRan = 1
     while True:
+
         newDevices = False
         print("Run #" + str(timesProgramRan))
         print("Searching...")
+
         nearby_devices = bluetooth.discover_devices(duration=2, lookup_names=True)
         print("Devices* in range" + str(nearby_devices))
+
+        # Loop that checks all unknown addresses and adds them to the list.
         for addr, name in nearby_devices:
             if(addr not in addressesArray):
                 addressesArray.append(addr)
+
             if(name not in namesArray):
                 print ("NEW DEVICE: " + str(name))
                 newDevices = True
@@ -31,6 +39,7 @@ def main():
         timesProgramRan = timesProgramRan + 1
         print()
         time.sleep(2)
+        """
 
 
 # update names and addresses        
